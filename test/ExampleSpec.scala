0648ba11b4c991422faf36e6b762cc5fce55695b
Rajkumar Natarajan <rajkumar.natarajan@synacor.com>
2020-12-04 14:20:40 -0500

    Fixed the issue when running scala test

--- test/ExampleSpec.scala
+++ test/ExampleSpec.scala
@@ -1,11 +1,14 @@
 import org.scalatest._
 import flatspec._
 import matchers._
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class ExampleSpec extends AnyFlatSpec with should.Matchers {
 
   "This Spec" should "be able to use both java and scala classes" in {
     val p = new Person(1, "desc")
-    val user = p.toUser
+    val user: User = p.toUser
   }
 }
